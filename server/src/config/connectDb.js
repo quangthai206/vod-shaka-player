f202b35f0db8c9a6aa835fa8f80c806c8c27b556
quangthai206 <laiquangthai206@gmail.com>
2020-12-12 15:55:12 +0700

    change db url

--- server/src/config/connectDb.js
+++ server/src/config/connectDb.js
@@ -3,8 +3,8 @@ const bluebird = require("bluebird");
 
 let connectDb = () => {
   mongoose.Promise = bluebird;
-  // let URI = `mongodb+srv://test123456:test123456@cluster0.v0t8i.mongodb.net/myapp?retryWrites=true&w=majority`;
-  let URI = `${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+  let URI = `mongodb+srv://test123456:test123456@cluster0.v0t8i.mongodb.net/myapp?retryWrites=true&w=majority`;
+  // let URI = `${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
   console.log(URI);
   console.log("connect db");
   return mongoose.connect(URI, {
